8d9bad29d09b74821066dd4026076db7fe7824f6
Ricky Bernal <rickyb@rickys-mbp.campus.csudh.edu>
2019-04-25 16:31:42 -0700

    Initial Commit

---
+++ TicChatToe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicChatToe
+//
+//  Created by Ricky Bernal on 4/25/19.
+//  Copyright © 2019 Ricky Bernal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
