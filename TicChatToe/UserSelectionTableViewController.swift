f80384e5ced0988b9b6f48fab3fe5a08c7560546
Hunter Boleman <hunter.boleman@gmail.com>
2019-05-02 14:08:51 -0700

    Made a table view controller for the tic tac toe, and transfered the connected username

--- TicChatToe/UserSelectionTableViewController.swift
+++ TicChatToe/UserSelectionTableViewController.swift
@@ -377,14 +377,19 @@ class UserSelectionTableViewController: UITableViewController {
      }
      */
     
-    /*
+    
      // MARK: - Navigation
      
      // In a storyboard-based application, you will often want to do a little preparation before navigation
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
      // Get the new view controller using segue.destination.
      // Pass the selected object to the new view controller.
+        
+        // Create a new variable to store the instance of PlayerTableViewController
+        let destinationVC = segue.destination as! TicTacToeTableViewController
+        destinationVC.connectedUser = self.selectedUser
      }
-     */
+    
+ 
     
 }
